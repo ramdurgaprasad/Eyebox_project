6f3aec53b1a5f72717677ae65c2cbcbc18bc3c7d
dinesh <dineshreddy1590@gmail.com>
2022-12-11 22:08:18 +0530

    reset password

--- src/main/java/com/example/Eyebox/controller/EyeBoxLogin.java
+++ src/main/java/com/example/Eyebox/controller/EyeBoxLogin.java
@@ -5,6 +5,7 @@ import com.example.Eyebox.entity.EyeBoxRegistrationEntity;
 import com.example.Eyebox.model.EyeBoxForgotPasswordModel;
 import com.example.Eyebox.model.EyeBoxRegistrationModel;
 import com.example.Eyebox.model.LoginRequestDTO;
+import com.example.Eyebox.model.ResetPassword;
 import com.example.Eyebox.service.EyeBoxRegistrationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,24 +21,28 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class EyeBoxLogin {
 
-        private EyeBoxRegistrationModel userDTO;
-        private LoginRequestDTO loginRequestDTO;
-        @Autowired
-        private EyeBoxRegistrationService userService;
+    private EyeBoxRegistrationModel userDTO;
+    private LoginRequestDTO loginRequestDTO;
+    @Autowired
+    private EyeBoxRegistrationService userService;
 
-       @PostMapping("/login")
-        public ResponseEntity<EyeBoxRegistrationModel> login(@RequestBody LoginRequestDTO loginRequestDTO) {
-            EyeBoxRegistrationEntity user = userService.login(loginRequestDTO.getUsername(), loginRequestDTO.getPassword());
-            return new ResponseEntity(user, HttpStatus.OK);
-        }
-
-
-        @PostMapping("/changepassword/{id}")
-        public ResponseEntity<String> login(@RequestBody EyeBoxForgotPasswordModel passwordModel, @PathVariable Long id) {
-            String res = userService.forgot(passwordModel,id);
-            return new ResponseEntity(res, HttpStatus.OK);
-        }
-
-
-
+    @PostMapping("/login")
+    public ResponseEntity<EyeBoxRegistrationModel> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        EyeBoxRegistrationEntity user = userService.login(loginRequestDTO.getUsername(), loginRequestDTO.getPassword());
+        return new ResponseEntity(user, HttpStatus.OK);
     }
+
+
+    @PostMapping("/changepassword/{id}")
+    public ResponseEntity<String> login(@RequestBody EyeBoxForgotPasswordModel passwordModel, @PathVariable Long id) {
+        String res = userService.forgot(passwordModel, id);
+        return new ResponseEntity(res, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/resetpassword/{id}")
+    public ResponseEntity<String> login(@RequestBody ResetPassword passwordModel, @PathVariable Long id) {
+        String res = userService.resetPassword(passwordModel, id);
+        return new ResponseEntity(res, HttpStatus.OK);
+    }
+}
